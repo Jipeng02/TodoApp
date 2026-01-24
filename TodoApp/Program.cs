4e78c14b0f95c7e5cd159eeb519629c24c1e3e85
wangjipeng1217 <wangjipeng1217@gmail.com>
2026-01-24 20:30:16 +0800

    init

---
+++ TodoApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public bool IsDone { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, TodoApp!");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(); // 等待用户按键
+    }
+}
